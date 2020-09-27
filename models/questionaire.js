63b21f857ea9a0c56285dd7e7721996088805779
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-09-27 16:56:01 -0700

    schema added for questionaire

---
+++ models/questionaire.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const questionaireSchema = new Schema({
+  breed: { type: String, required: true },
+  size: { type: Interger, required: true },
+  age: { type: Interger, required: true },
+  energyLevel: { type: Interger, required: true },
+});

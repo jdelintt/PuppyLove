df8ec41e03a67751f146181d50eeda5d87576ff0
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-09-27 17:23:36 -0700

    questionaireController - needs to be edited

---
+++ controllers/questionaireController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+
+// interacting with database
+module.exports = {
+  findAll: function (req, res) {
+    db.Questionaire.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Questionaire.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Questionaire.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Questionaire.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Questionaire.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  questionaire: require("./questionaire"),
+  Questionaire: require("./questionaire"),
 };

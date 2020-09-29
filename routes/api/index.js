7897b853c4f52affd152b2063978fdad6ca1e4e9
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-09-29 11:37:07 -0700

    changes in schema + controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userInfoRoutes = require("./userInfo");
+const petfinderRoutes = require("./petfinder");
 
 // Book routes
 router.use("/preference", userInfoRoutes);
+router.use("/petfinder", petfinderRoutes);
 
 module.exports = router;

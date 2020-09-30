00bf3ad737ba3b1b60227d2c1c9b56591302c429
AileenWang <ainiwang14@gmail.com>
2020-09-29 20:03:02 -0700

    drafted up background check pg

---
+++ client/src/pages/BackgroundChk/BackgroundChk.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Form, Button} from "react-bootstrap"
+import Container from "../../components/Container"
+
+function SubmitInfo(){
+  return (
+<Container style={{ maxWidth:"600px"}}>
+  <h1>Puppy Love</h1>
+  <p>Puppies are a lot of responsibility! Before we dive in to all the puppies available for lovin' in your area, please give us your first and last legal name so we may do a cursory background check. We will never sell, appropriate, or abuse your information.</p>
+<Form>
+  <Form.Group controlId="formFirstName">
+    <Form.Label>First Name</Form.Label>
+    <Form.Control type="firstName" placeholder="First Name" />
+  </Form.Group>
+
+  <Form.Group controlId="formLastName">
+    <Form.Label>Last Name</Form.Label>
+    <Form.Control type="lastName" placeholder="Last Name" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</Container>
+  )
+}
+export default SubmitInfo;

a21ff77937e9ebe16a777445f73976231f1b16ea
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-09-30 10:41:11 -0700

    frontend API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,3 +3,13 @@
 var axios = require("axios");
 
 axios.get("api/userinfo", function (req, res) {});
+
+export default {
+  findAll: function () {
+    return axios.get("/api/userinfo");
+  },
+
+  createPref: function (data) {
+    axios.post("/api/userinfo", data);
+  },
+};

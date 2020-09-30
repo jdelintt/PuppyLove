3b53cccdb7d7ac417dcdf14eda06e9f8624fa511
AileenWang <ainiwang14@gmail.com>
2020-09-30 11:44:30 -0700

    added birthdate form and took off form label

--- client/src/pages/BackgroundChk/BackgroundChk.js
+++ client/src/pages/BackgroundChk/BackgroundChk.js
@@ -6,16 +6,18 @@ function BackgroundChk(){
   return (
 <Container style={{ maxWidth:"600px"}}>
   <h1>Puppy Love</h1>
-  <p>Puppies are a lot of responsibility! Before we dive in to all the puppies that need lovin' in your area, please give us your first and last legal name so we may do a cursory background check. We will never sell, appropriate, or abuse your information.</p>
+  <p>Puppies are a lot of responsibility! Before we dive in to all the puppies that need lovin' in your area, please give us a bit more information so we may do a cursory background check. We will never sell, appropriate, or abuse your information.</p>
 <Form>
   <Form.Group controlId="formFirstName">
-    <Form.Label>First Name</Form.Label>
     <Form.Control type="firstName" placeholder="First Name" />
   </Form.Group>
 
   <Form.Group controlId="formLastName">
-    <Form.Label>Last Name</Form.Label>
     <Form.Control type="lastName" placeholder="Last Name" />
+  </Form.Group>
+
+  <Form.Group controlId="formBirthdate">
+    <Form.Control type="birthDate" placeholder="Birth Date" />
   </Form.Group>
 
   <Button variant="primary" type="submit">

749273e6cb77ec9211f9476f3a3af6c8195c68a5
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-09-30 11:59:07 -0700

    added state + onsubmit

--- client/src/pages/Questionaire/Questionaire.js
+++ client/src/pages/Questionaire/Questionaire.js
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
 import "./Questionaire.css";
-const energyLevels = ['Mellow', 'Moderate', 'High', 'Hyper'];
-const allergies = ['Short', 'Medium', 'Long']
-
+const energyLevels = ["Mellow", "Moderate", "High", "Hyper"];
+const allergies = ["Short", "Medium", "Long"];
 
 export default () => {
   const [prefs, setPrefs] = useState({
-      size: 5,
-      age: 1,
-      energy: 0,
-      gender: 0,
-      allergies: 0
+    size: 5,
+    age: 1,
+    energy: 0,
+    gender: 0,
+    allergies: 0,
   });
+
+  //data
+  const [APIdata, setData] = useState({
+    breed: "",
+    malePref: "",
+    femalePref: "",
+    size: "",
+    age: "",
+    energyLevel: "",
+    allergies: "",
+  });
+
+  const onSubmit = () => {
+    console.log(APIdata);
+  };
+
   const handleInputChange = ({ target: { name, value } }) =>
     setPrefs({ ...prefs, [name]: value });
   console.log(prefs);
@@ -24,18 +39,18 @@ export default () => {
         <form>
           <div class="form-group">
             <label for="breedInput">Select Breed</label>
-            <select
+            <datalist
               name="breed"
+              value={setData.breed}
               onChange={handleInputChange}
               class="form-control"
-              id="breedInput"
-            >
+              id="breedInput">
               <option>Golden Retriever</option>
               <option>Pitbull</option>
               <option>German Shepherd</option>
               <option>Chihuahua</option>
               <option>Terrier</option>
-            </select>
+            </datalist>
           </div>
           <legend>Gender</legend>
           <div className="form-group">
@@ -44,6 +59,7 @@ export default () => {
                 type="checkbox"
                 className="custom-control-input"
                 id="customCheck1"
+                value={setData.malePref}
               />
               <label className="custom-control-label" for="customCheck1">
                 Male
@@ -54,9 +70,10 @@ export default () => {
                 type="checkbox"
                 className="custom-control-input"
                 id="customCheck2"
+                value={setData.femalePref}
               />
               <label className="custom-control-label" for="customCheck2">
-               Female
+                Female
               </label>
             </div>
           </div>
@@ -64,7 +81,7 @@ export default () => {
             <legend>Size</legend>
             <label for="sizeSlide">{prefs.size} LBS</label>
             <input
-            defaultValue="5"
+              defaultValue="5"
               name="size"
               onChange={handleInputChange}
               min="5"
@@ -72,6 +89,7 @@ export default () => {
               type="range"
               className="custom-range"
               id="sizeSlide"
+              value={setData.size}
             />
           </div>
           <div className="sliders">
@@ -86,6 +104,7 @@ export default () => {
               type="range"
               className="custom-range"
               id="ageSlide"
+              value={setData.age}
             />
           </div>
           <div className="sliders">
@@ -100,6 +119,7 @@ export default () => {
               type="range"
               className="custom-range"
               id="energySlide"
+              value={setData.energyLevel}
             />
           </div>
           <div className="sliders">
@@ -114,6 +134,7 @@ export default () => {
               type="range"
               className="custom-range"
               id="alergiesSlide"
+              value={setData.allergies}
             />
           </div>
         </form>
